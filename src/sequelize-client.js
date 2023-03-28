09af2474280f1ac2ac913d8dc8e1fdd9a95886da
Etudiant O'clock <mike.dutheil@gmail.com>
2023-03-28 09:36:44 +0200

    Version Propre

--- src/sequelize-client.js
+++ src/sequelize-client.js
@@ -1,12 +1,10 @@
-const dotenv = require("dotenv/config");
-const pg = require('pg');
+require("dotenv/config");
 
 const { Sequelize } = require('sequelize');
 
 // Ceci est une instance de connexion à la BDD Postgres (c'est notre "client")
-const sequelize = new Sequelize(process.env.PG_URL, { 
-  dialect: 'postgres',
-  dialectModule: pg,
+const sequelize = new Sequelize(process.env.PG_URL, {
+  dialect: "postgres",
   define: {
     createdAt: "created_at", // Dans notre table, le champ 'createdAt' s'appelle `create_at`
     updatedAt: "updated_at" // Comme on va avoir plusieurs models, on peut aussi mettre cette liaison directement dans l'instance sequelize
