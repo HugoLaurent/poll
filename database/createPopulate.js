28e452b7330a20df9e75497ef4fe932b25ae5c53
HugoLaurent <lauhugopro@gmail.com>
2023-03-29 18:20:57 +0200

    salut

--- database/createPopulate.js
+++ database/createPopulate.js
@@ -1,17 +1,9 @@
 const dotenv = require("dotenv");
 const path = require("path");
 
-<<<<<<< HEAD
 dotenv.config({
 path:path.resolve(__dirname,"../.env")
 });
-=======
-
-dotenv.config({
-  path:path.resolve(__dirname,"../.env")
-});
-
->>>>>>> refs/remotes/origin/main
 
 const { User, Poll, Category } = require('../src/models');
 
